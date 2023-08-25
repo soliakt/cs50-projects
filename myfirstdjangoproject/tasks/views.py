0839e07a9efa69a5bf3cf9243b03e4590cf29636
Victor Gimeno <soliakt4@gmail.com>
2023-08-25 13:46:45 +0200

    tasks and cookies

--- myfirstdjangoproject/tasks/views.py
+++ myfirstdjangoproject/tasks/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
+
 # Create your views here.
 def index(request):
+    # Las dos siguientes lineas significan: Si el usuario entra por primera vez a la pagina, que tenga la lista vacía
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
