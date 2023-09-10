52370f2d1c78481a620f4a77f8988ec9e17819f4
Victor Gimeno <soliakt4@gmail.com>
2023-09-10 14:37:59 +0200

    Form accepts entries

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# This app_name is not acc needed bc we only have 1 app,
+# but it's worth having in case we have multiple apps.
+# What it makes is, in case we add a link, it lets Django 
+# find out what app you're refering to when linking.
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add", views.add, name="add")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    task = forms.CharField(label="New Entry")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def add(request):
+    return render(request, "encyclopedia/add.html",{
+        "form": NewEntryForm()
+    })
