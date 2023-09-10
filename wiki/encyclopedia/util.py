246dfb09a3e0d1085304a4159622af2f8066c12d
Victor Gimeno <soliakt4@gmail.com>
2023-09-10 17:30:28 +0200

    Entries get added successfully and stored

--- wiki/encyclopedia/util.py
+++ wiki/encyclopedia/util.py
@@ -9,8 +9,10 @@ def list_entries():
     Returns a list of all names of encyclopedia entries.
     """
     _, filenames = default_storage.listdir("entries")
-    return list(sorted(re.sub(r"\.md$", "", filename)
+    entry_list = list(sorted(re.sub(r"\.md$", "", filename)
                 for filename in filenames if filename.endswith(".md")))
+    
+    return entry_list
 
 
 def save_entry(title, content):
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 
 from . import util
 
+entries = []
 class NewEntryForm(forms.Form):
-    task = forms.CharField(label="New Entry")
+    entry = forms.CharField(label="New Entry")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,6 +13,17 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+           entry = form.cleaned_data["entry"] 
+           # Esto guarda la entrada utilizando la función save_entry de util
+           util.save_entry(entry, "")
+           return redirect("encyclopedia/index.html")
+        else:
+            return render(request, "encyclopedia/add.html",{
+                "form": form
+            })
     return render(request, "encyclopedia/add.html",{
         "form": NewEntryForm()
     })
